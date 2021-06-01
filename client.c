b02feb5a89e8ebf78df124159bef12afde1b69ac
Mko27 <85189917+Mko27@users.noreply.github.com>
2021-06-01 21:18:00 +0300

    Update client.c

--- client.c
+++ client.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 	int their_sock;
 	int their_addr_size;
 	int portno;
-	pthread_t recvt;
+	pthread_t sendt,recvt;
 	char msg[500];
 	char username[100];
 	char res[600];
