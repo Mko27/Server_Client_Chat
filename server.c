7d23128d1589bdb072f4401d33522fcdb7c1cf32
Mko27 <85189917+Mko27@users.noreply.github.com>
2021-06-01 21:06:38 +0300

    MultiClient chat

---
+++ server.c
@@ -0,0 +1,123 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+struct client_info {
+	int sockno;
+	char ip[INET_ADDRSTRLEN];
+};
+
+int clients_count[100];
+int n = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+void send_to_all(char *msg,int curr)
+{
+	int i;
+	pthread_mutex_lock(&mutex);
+	for(i = 0; i < n; i++) {
+		if(clients_count[i] != curr) {
+			if(send(clients_count[i],msg,strlen(msg),0) < 0) {
+				perror("sending failure");
+				continue;
+			}
+		}
+	}
+	pthread_mutex_unlock(&mutex);
+}
+
+
+void *recv_msg(void *sock)
+{
+	struct client_info cl = *((struct client_info *)sock);
+	char msg[500];
+	int length;
+	int i;
+	int j;
+	while((length = recv(cl.sockno,msg,500,0)) > 0) {
+		msg[length] = '\0';
+		send_to_all(msg,cl.sockno);
+		memset(msg,'\0',sizeof(msg));
+	}
+	pthread_mutex_lock(&mutex);
+	printf("%s disconnected\n",cl.ip);
+	for(i = 0; i < n; i++) {
+		if(clients_count[i] == cl.sockno) {
+			j = i;
+			while(j < n-1) {
+				clients_count[j] = clients_count[j+1];
+				j++;
+			}
+		}
+	}
+	n--;
+	pthread_mutex_unlock(&mutex);
+}
+
+
+int main(int argc,char *argv[])
+{
+    struct sockaddr_in my_addr,their_addr;
+	int my_sock;
+	int their_sock;
+	socklen_t their_addr_size;
+	int portno;
+	pthread_t sendt,recvt;
+	char msg[500];
+	int len;
+	struct client_info cl;
+	char ip[INET_ADDRSTRLEN];
+
+	if(argc > 2) {
+		printf("too many arguments");
+		exit(1);
+	}
+    portno = atoi(argv[1]);
+	my_sock = socket(AF_INET,SOCK_STREAM,0);
+	memset(my_addr.sin_zero,'\0',sizeof(my_addr.sin_zero));
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(portno);
+	my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	their_addr_size = sizeof(their_addr);
+
+	if(bind(my_sock,(struct sockaddr *)&my_addr,sizeof(my_addr)) != 0) {
+		perror("binding unsuccessful");
+		exit(1);
+	}
+
+	if(listen(my_sock,5) != 0) {
+		perror("listening unsuccessful");
+		exit(1);
+	}
+
+	while(1) {
+		if((their_sock = accept(my_sock,(struct sockaddr *)&their_addr,&their_addr_size)) < 0) {
+			perror("accept unsuccessful");
+			exit(1);
+		}
+	    pthread_mutex_lock(&mutex);
+		inet_ntop(AF_INET, (struct sockaddr *)&their_addr, ip, INET_ADDRSTRLEN);
+		printf("%s connected\n",ip);
+		cl.sockno = their_sock;
+		strcpy(cl.ip,ip);
+		clients_count[n] = their_sock;
+		n++;
+		pthread_create(&recvt,NULL,recv_msg,&cl);
+		pthread_mutex_unlock(&mutex);
+	}
+	return 0;
+}
+
+/*
+for compile
+gcc filename.c -pthread
+./a.out portnumber
+./a.out username portnumber
+*/
